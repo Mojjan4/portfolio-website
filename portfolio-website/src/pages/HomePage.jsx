e0f4dd65a15b968f98413ae2d8a2667a592b891d
Christopher.Lundqvist <christopher.lundqvist@ecarxgroup.com>
2025-03-26 15:36:57 +0100

    draft layout

--- portfolio-website/src/pages/HomePage.jsx
+++ portfolio-website/src/pages/HomePage.jsx
@@ -6,15 +6,31 @@ const HomePage = () => {
     <Box
       sx={{
         display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        height: "100vh",
-        padding: "200px",
+        flexDirection: "column",
+        alignItems: "flex-start", // Align content to the left
+        marginLeft: "2rem", // Adjust spacing from the left edge
+        marginTop: "2rem", // Adjust spacing from the top
+        gap: 2, // Space between elements
       }}
     >
       <Typography variant="h4" color="white">
         Welcome to My Digital Portfolio
       </Typography>
+
+      {/* Second Text Inside Rounded Rectangle */}
+      <Box
+        sx={{
+          borderInlineColor: "white",
+          border: "1px solid white",
+          padding: "10px 20px",
+          borderRadius: "10px", // Rounded corners
+          display: "inline-block",
+        }}
+      >
+        <Typography variant="h6">
+          Christopher Lundqvist / Software Developer
+        </Typography>
+      </Box>
     </Box>
   );
 };
